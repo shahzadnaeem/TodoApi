f2c60855ce91b3409239cab72b568997d3c0abe6
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-02-08 14:53:35 +0000

    Clean up

--- TodoApi/Extensions/DatabaseExtensions.cs
+++ TodoApi/Extensions/DatabaseExtensions.cs
@@ -15,16 +15,10 @@ public static class DatabaseExtensions
         var config = builder.Configuration;
 
         var dbProvider = config.GetValue("DbProvider", SQLITE);
-        Console.WriteLine($"Got Database Provider: '{dbProvider}'");
-
-        // NOTE: Need to keep separate Migrations projects for each DB type
-        //       Postgres does not like Sqlite limitations on bool columns stored as INTEGER
 
         if (dbProvider == SQLITE)
         {
             var connectionString = builder.Configuration.GetConnectionString(dbProvider);
-            System.Console.WriteLine($"# Got Sqlite connection string: '{connectionString}'");
-
             var assembly = typeof(Migrations.Sqlite.Identifier).Assembly.GetName().Name;
 
             builder.Services.AddDbContext<TodoDbContext>(options =>
@@ -37,6 +31,7 @@ public static class DatabaseExtensions
         else if (dbProvider == POSTGRES)
         {
             var connectionString = builder.Configuration.GetConnectionString(dbProvider);
+            var assembly = typeof(Migrations.Postgres.Identifier).Assembly.GetName().Name;
 
             if (builder.Environment.IsDevelopment())
             {
@@ -49,10 +44,6 @@ public static class DatabaseExtensions
 
                 connectionString = cb.ConnectionString;
             }
-
-            System.Console.WriteLine($"# Got Postgres connection string: '{connectionString}' - Password from user-secrets");
-
-            var assembly = typeof(Migrations.Postgres.Identifier).Assembly.GetName().Name;
 
             builder.Services.AddDbContext<TodoDbContext>(options =>
             {
